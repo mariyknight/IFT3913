b63e264ee88895904d0c035d2bf0e5ef0d4a2f29
mariyknight <huangweijia0808@gmail.com>
2023-09-26 20:24:42 -0400

    tloc update

---
+++ tp1/src/main/java/tloc.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class tloc {
+    public static void main(String[] args) {
+
+    }
+
+    public static int calculateTloc(String filePath) throws IOException {
+        int tloc = 0;
+        return tloc;
+    }
+}

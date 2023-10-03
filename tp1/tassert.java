fbe1415db57f05be5648c44ea557d3a53fa8f9c5
mariyknight <huangweijia0808@gmail.com>
2023-10-03 19:17:38 -0400

    tassert update

--- tp1/tassert.java
+++ tp1/tassert.java
@@ -1,3 +1,75 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class tassert {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("");
+            return;
+        }
+
+        String filePath = args[0];
+
+        try{
+            int tloc = calculateTassert(filePath);
+            System.out.println("Nombre d'assert :" + tloc);
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+        }
+    }
+
+    public static int calculateTassert(String filePath) throws IOException {
+        int tassert = 0;
+
+         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
     
+            String line;
+
+            while ((line = br.readLine()) != null){
+                line = line.trim();
+                if(line.contains("assert")){
+                    tassert++;
+                }
+                else if(line.contains("assertTrue")){
+                    tassert++;
+                }
+                else if(line.contains("assertFalse")) {
+                    tassert++;
+                }
+                else if(line.contains("assertArrayEquals")) {
+                    tassert++;
+                }
+                else if(line.contains("assertEquals")) {
+                    tassert++;
+                }
+                else if(line.contains("assertNotEquals")) {
+                    tassert++;
+                }
+                else if(line.contains("assertNotNull")) {
+                    tassert++;
+                }
+                else if(line.contains("assertNotSame")) {
+                    tassert++;
+                }
+                else if(line.contains("assertNull")) {
+                    tassert++;
+                }
+                else if(line.contains("assertSame")) {
+                    tassert++;
+                }
+                else if(line.contains("assertThat")) {
+                    tassert++;
+                }
+                else if(line.contains("assertThrows")) {
+                    tassert++;
+                }
+                else if(line.contains("fail")) {
+                    tassert++;
+                }
+            }
+        }
+        return tassert;
+    }
 }

5b7ccd882509b3a4076077d3d0a327b760b08293
zakataha <75871055+zakataha@users.noreply.github.com>
2023-10-05 23:36:25 -0400

    Update tropcomp.java
    reste commentaires  + response.txt

--- tp1/tropcomp.java
+++ tp1/tropcomp.java
@@ -1,63 +1,97 @@
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
-//----not working yet need % / seuil calculation and process project paths... 
-public class tropcomp {public static void main(String[] args) throws Exception {
+import java.util.stream.Collectors;
+//marche mais reste commentaires + response.txt, il pr seuil = 10 -> tropcomp retourn 3 results ; pr 1 et 5 ->retourne 0 result.
+public class tropcomp {
+    public static void main(String[] args) throws Exception {
         if (args.length < 2 || args.length > 4) {
             System.out.println("Veuillez entrer dans le terminal: java tropcomp [-o <path_output.csv>] <path_to_main_project> <threshold>");
             System.exit(1);
         }
-        
-        String outputPath = null;
-        String inputPath;
-        double threshold = 0;
+
+        String sortie = null;
+        String cheminProjet;
+        double seuil;
 
         if (args[0].equals("-o")) {
-            outputPath = args[1];
-            inputPath = args[2];
-            threshold = Double.parseDouble(args[3]);
+            sortie = args[1];
+            cheminProjet = args[2];
+            seuil = Double.parseDouble(args[3]);
         } else {
-            inputPath = args[0];
-            threshold = Double.parseDouble(args[1]);
+            cheminProjet = args[0];
+            seuil = Double.parseDouble(args[1]);
         }
-        
-        try{
-            List<ClassMetrics> metricsList = getMetricsFromDirectory(inputPath);
-            metricsList = metricsList.stream()
-                                      .filter(m -> isComplexe(m, threshold))
-                                      .collect(Collectors.toList());
 
-            try (PrintStream output = (outputPath != null) ? new PrintStream(new FileOutputStream(outputPath)) : System.out) {
-                metricsList.stream().forEach(m -> 
-                    output.printf("%s, %s, %s, %d, %d, %.2f%n", m.relativePath, m.packageName, m.className, m.tloc, m.tassert, m.tcmp)
-                );
-                output.close();
-            }       
-        } catch (Exception ex) {
+       try (PrintStream output = (sortie != null) ? new PrintStream(new FileOutputStream(sortie)) : System.out) {
+            List<ClassMetrics> metricsList = getMetricsFromDirectory(Paths.get(cheminProjet));
+            int tlocSeuil = getSeuilTloc(metricsList, seuil);
+            double tcmpSeuil = getSeuilTcmp(metricsList, seuil);
+
+            metricsList = metricsList.stream()
+                                     .filter(m -> m.tloc >= tlocSeuil && m.tcmp >= tcmpSeuil)
+                                     .collect(Collectors.toList());
+            for (ClassMetrics metrics : metricsList) {
+                output.printf("%s, %d, %.2f%n", metrics.relativePath, metrics.tloc, metrics.tcmp);
+            }
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-
-    // collect class metrics of all java files in the directory and its subdirectories
-    static List<ClassMetrics> getMetricsFromDirectory(String inputPath) {
+    
+    static List<ClassMetrics> getMetricsFromDirectory(Path projectPath) throws IOException {
+        List<ClassMetrics> metricsList = new ArrayList<>();
         
-        // Return metrics in a list
-        return new ArrayList<>();
+        Files.walk(projectPath)
+             .filter(Files::isRegularFile)
+             .filter(p -> p.toString().endsWith(".java"))
+             .forEach(p -> {
+                try {
+                    int tlocValue = tloc.calculateTloc(p.toString());
+                    int tassertValue = tassert.calculateTassert(p.toString());
+                    double tcmp = tassertValue != 0 ? (double) tlocValue / tassertValue : 0;
+                    metricsList.add(new ClassMetrics(p.toString(), tlocValue, tassertValue, tcmp));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+             });
+
+        return metricsList;
     }
 
-    // check if a class is suspected to be complex (based on its metrics)
-    static boolean maybeComplex(ClassMetrics metrics, double threshold) {
-        // heuristic...
-        // return true if both TLOC and TCMP are in the top `threshold` percent of all classes
-        return false;
+    private static double getSeuilTcmp(List<ClassMetrics> metricsList, double threshold) {
+        double[] tcmpValues = new double[metricsList.size()];
+        for (int i = 0; i < metricsList.size(); i++) {
+            tcmpValues[i] = metricsList.get(i).tcmp;
+        }
+        Arrays.sort(tcmpValues);
+        int index = (int) (metricsList.size() * (1 - threshold / 100.0));
+        return (index < tcmpValues.length) ? tcmpValues[index] : 0.0;
     }
-
+    
+    private static int getSeuilTloc(List<ClassMetrics> metricsList, double threshold) {
+        int[] tlocValues = new int[metricsList.size()];
+        for (int i = 0; i < metricsList.size(); i++) {
+            tlocValues[i] = metricsList.get(i).tloc;
+        }
+        Arrays.sort(tlocValues);
+        int index = (int) (metricsList.size() * (1 - threshold / 100.0));
+        return (index < tlocValues.length) ? tlocValues[index] : 0;
+    }
+    
+    
+    
     static class ClassMetrics {
         String relativePath;
-        String packageName;
-        String className;
         int tloc;
         int tassert;
         double tcmp;
+
+        public ClassMetrics(String relativePath, int tloc, int tassert, double tcmp) {
+            this.relativePath = relativePath;
+            this.tloc = tloc;
+            this.tassert = tassert;
+            this.tcmp = tcmp;
+        }
     }
 }

a8bc0bd84e70cfff3fd5b684b741a964d2acde82
zakataha <75871055+zakataha@users.noreply.github.com>
2023-10-26 21:56:34 -0400

    Create TestDocumentationMetrics.java
    code pour répondre à Q4 -> Est-ce que les tests sont suffisamment documentés?
    
    output : Average Test Comment Density (M7): 102.43%

---
+++ tp2/TestDocumentationMetrics.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestDocumentationMetrics {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: java TestDocumentationMetrics <path_to_test_directory>");
+            System.exit(1);
+        }
+
+        String testDirPath = args[0];
+        List<TestClassMetrics> metricsList = new ArrayList<>();
+
+        // Retrieve metrics for all test files
+        Files.walk(Paths.get(testDirPath))
+                .filter(Files::isRegularFile)
+                .filter(p -> p.toString().endsWith("Test.java"))
+                .forEach(p -> {
+                    try {
+                        int tlocValue = tloc.calculateTloc(p.toString());
+                        int clocValue = calculateCloc(p);
+                        metricsList.add(new TestClassMetrics(p, tlocValue, clocValue));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        // Compute average DC (Comment Density)
+        double averageDC = metricsList.stream().filter(m -> m.tloc != 0).mapToDouble(m -> (double) m.cloc / m.tloc).average().orElse(Double.NaN);
+        double average_percent = averageDC * 100; // Convert average ratio to percentage
+        System.out.printf("Average Test Comment Density (M7): %.2f%%\n", average_percent);
+
+    }
+
+    // Calculate number of comment lines (CLOC) in a file
+    public static int calculateCloc(Path path) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path.toFile()))) {
+            String line;
+            int commentLineCount = 0;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (line.startsWith("//") || line.startsWith("/*") || line.startsWith("*")) {
+                    commentLineCount++;
+                }
+            }
+            return commentLineCount;
+        }
+    }
+
+    private static class TestClassMetrics {
+        private final Path path;
+        private final int tloc;
+        private final int cloc;
+
+        public TestClassMetrics(Path path, int tloc, int cloc) {
+            this.path = path;
+            this.tloc = tloc;
+            this.cloc = cloc;
+        }
+    }
+}
